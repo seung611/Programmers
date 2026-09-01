c05814b53995dd89fab4edb7149fc0da611459a8
seung611 <a46070766@gmail.com>
2026-09-01 16:53:45 +0900

    [level 0] Title: 개미 군단, Time: 0.09 ms, Memory: 45.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int hp) {
+        int a = hp / 5;
+        int b = (hp - (a * 5)) / 3;
+        int c = (hp - (a * 5) - (b * 3));
+        return a + b + c;
+    }
+}
