bbfe6c2c44b3444ab9c657f2004f89c978a3599e
seung611 <a46070766@gmail.com>
2026-09-01 10:04:56 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 1.28 ms, Memory: 47 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Array.Sort(numbers);
+        Array.Reverse(numbers);
+        answer = numbers[0] * numbers[1];
+        return answer;
+    }
+}
