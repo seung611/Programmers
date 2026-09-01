b26c84a4f9613ccdcf5cf9a2a8b1a1ddb2f5f4d1
seung611 <a46070766@gmail.com>
2026-09-01 09:36:08 +0900

    [level 0] Title: n의 배수 고르기, Time: 3.17 ms, Memory: 46.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = numlist.Where(x => x % n == 0).ToArray();
+        return answer;
+    }
+}
