356c28f7f21120383319913c1834dc3939cc11a3
seung611 <a46070766@gmail.com>
2026-09-07 09:10:44 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 1.36 ms, Memory: 45.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+        Array.Sort(num_list);
+        for (int i = 0; i < 5; i++)
+        {
+            answer[i] = num_list[i];
+        }
+        return answer;
+    }
+}
